b7dd61e474138bc158ffc86db1869901938bbd9c
Mauro Lepore <maurolepore@gmail.com>
2024-10-25 18:15:41 -0600

    Update app.R

--- src/app.R
+++ src/app.R
@@ -2,7 +2,6 @@ library(shiny)
 
 # Installing private packages here because GITHUB_PAT isnt' discovered
 # when installing in setup.R or Dockerfile. I don't know why.
-
 pak::pak("maurolepore/private")
 
 ui <- fluidPage(
